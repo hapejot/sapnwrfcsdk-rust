7c75bc57f547e71c76436fc1511ca5e2f21c4274
Peter Jaeckel <pjl@inwerken.de>
2025-12-09 17:37:13 +0100

    fix: Update get_output_path to use current directory instead of manifest dir

--- build.rs
+++ build.rs
@@ -89,9 +89,9 @@ fn copy_dll(dest_path: &PathBuf, name: &str, output_path: &PathBuf) {
 
 fn get_output_path() -> PathBuf {
     //<root or manifest path>/target/<profile>/
-    let manifest_dir_string = env::var("CARGO_MANIFEST_DIR").unwrap();
+    // let manifest_dir_string = env::var("CARGO_MANIFEST_DIR").unwrap();
     let build_type = env::var("PROFILE").unwrap();
-    let path = Path::new(&manifest_dir_string)
+    let path = Path::new(".")
         .join("target")
         .join(build_type);
     return PathBuf::from(path);
